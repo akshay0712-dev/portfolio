ec0dfa51ed5dbe2e8ecf5584d9b7c79f42984834
AkshayGit <akshayrishu4@gmail.com>
2024-11-01 23:38:10 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Intro from "./components/Intro";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
